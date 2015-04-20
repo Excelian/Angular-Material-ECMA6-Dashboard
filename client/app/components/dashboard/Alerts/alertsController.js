348d418a692ea9e4afb2468ed0b51676f582bd38
hughest <tim.hughes@excelian.com>
2015-04-20 13:26:41 +0100

    simulate alert events occurring

--- client/app/components/dashboard/Alerts/alertsController.js
+++ client/app/components/dashboard/Alerts/alertsController.js
@@ -1,11 +1,74 @@
-export default class alertsController{
-  constructor($scope) {
+export default class alertsController {
+  constructor($scope,$interval,$filter) {
+
+    $scope.displayControl = {filter:"",displayFrozen:false};
     $scope.alerts = [];
 
-    $scope.alerts.push({type: 'error', ts: '2015-04-17 17:00:01', description: 'Trade Services connection lost'});
-    $scope.alerts.push({type: 'warning', ts: '2015-04-17 16:52:23', description: 'Auth Services response timeout, retrying.'});
-    $scope.alerts.push({type: 'error', ts: '2015-04-17 16:00:01', description: 'Trade Services connection lost'});
-    $scope.alerts.push({type: 'warning', ts: '2015-04-17 15:52:23', description: 'Auth Services response timeout, retrying.'});
-    $scope.alerts.push({type: 'warning', ts: '2015-04-17 15:42:23', description: 'Auth1 Services response timeout, retrying.'});
+    $scope.alerts.push({type: 'error', ts: '2015-04-17 17:00:01', description: 'Trade Services connection lost.'});
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 16:52:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+    $scope.alerts.push({type: 'error', ts: '2015-04-17 16:00:01', description: 'Trade Services connection lost.'});
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:52:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:42:23',
+      description: 'Auth1 Services response timeout, retrying.'
+    });
+    $scope.alerts.push({type: 'error', ts: '2015-04-17 17:00:01', description: 'Trade Services connection lost.'});
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 16:52:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+    $scope.alerts.push({type: 'error', ts: '2015-04-17 16:00:01', description: 'Trade Services connection lost.'});
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:52:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:42:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:42:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+
+    $scope.alerts.push({
+      type: 'warning',
+      ts: '2015-04-17 15:42:23',
+      description: 'Auth Services response timeout, retrying.'
+    });
+
+    // Periodically add a new event to the array
+    function logTick() {
+
+      if ($scope.data.displayFrozen){
+        return;
+      }
+      var t = $filter('date')(new Date(), 'yyyy-MM-dd hh:mm:ss');
+
+      $scope.alerts.unshift({
+        type: 'warning',
+        ts: t,
+        description: 'Another Service warning message.'
+      });
+    }
+
+    var logTickTimer = $interval( logTick,5000 );
+
+    $scope.$on('$destroy', function(){
+      $interval.cancel(logTickTimer);
+    })
   }
 }
