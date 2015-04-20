8ae1184171e09c97a6a4173602aa8364d67d7ae0
tusharafernando <tushara.fernando@excelian.com>
2015-04-20 15:24:30 +0100

    travis

--- config/karma.config.js
+++ config/karma.config.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
     frameworks: ['jasmine', 'traceur'],
     browsers: ['Chrome'],
     reporters: ['progress','coverage'],
-    plugins: ['karma-jasmine','karma-traceur-preprocessor','karma-coverage','karma-chrome-launcher','karma-phantomjs-launcher'],
+    plugins: ['karma-jasmine','karma-traceur-preprocessor','karma-coverage','karma-chrome-launcher','karma-firefox-launcher'],
     files: [
       'components/es6-module-loader/dist/es6-module-loader.src.js',
       'components/system.js/dist/system.src.js',
